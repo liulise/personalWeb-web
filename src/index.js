909d54a98fbd4a6cf452442037dda77bbf4630aa
liulise <980168151@qq.com>
2017-12-02 23:02:07 +0800

    base scaffold

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import './assets/css/index.css';                    // 使用 css-loader 只能处理相对路径的文件
+import './assets/css/key.css';
+
+// 在js中如果想要获取图片地址，只能手动import，所以尽量使用背景和CDN
+import imgSrc from "./assets/images/bg.jpg";
+
 const a = 123;
+console.log(imgSrc);
--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,53 @@ const path = require('path');
 const config = require('./config');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 const copyWebpackPlugin = require('copy-webpack-plugin');
+const extractTextPlugin = require('extract-text-webpack-plugin');
+const extractPlugin = new extractTextPlugin({ filename: 'static/css/[name].css' });
 
 module.exports = {
   entry: path.join(config.rootSrc, 'index.js'),
 
   output: {
-    path: config.distSrc,
+    path: config.distSrc,                                   // webpack打包的模块均以该值作为目录
     filename: 'js/[name].js',
     chunkFilename: "js/[id].js",
-    publicPath: '',
+    publicPath: '/',
   },
 
   module: {
     rules: [
+      // js loader
       {
         test: /\.js$/,
         loader: "babel-loader",
-        include: [config.rootSrc]
+        include: [config.rootSrc],
+        exclude: [/node_modules/]
+      },
+
+      // css loader
+      {
+        test: /\.css$/,
+        include:[config.rootSrc] ,
+        use: extractPlugin.extract({
+          use: [
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: { plugins: [require("autoprefixer")()] }
+            }
+          ],
+          fallback: "style-loader"
+        }),
+      },
+
+      // img loader
+      {
+        test: /\.(png|gif|jpg|jpeg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 9000,
+          name: 'static/images/[name].[ext]'
+        },
       }
     ]
   },
@@ -32,6 +62,17 @@ module.exports = {
     new copyWebpackPlugin([{
       to: path.resolve(config.distSrc, 'static'),
       from: path.resolve(config.rootSrc, 'static')
-    }])
-  ]
+    }]),
+
+    extractPlugin
+  ],
+
+  devServer: {
+    port: 9000,
+    host: 'localhost',
+    overlay: {
+      errors: true,                 // 打包出现错误时，将错误显示在浏览器端
+      warnings: true,               // 打包出现警告时，将错误显示在浏览器端
+    },
+  }
 };
